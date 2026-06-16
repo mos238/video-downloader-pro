36f103e7858a86c9998d027c8fc12629c6723c6b
zeus <m.omarsayed@gmail.com>
2026-06-16 19:08:53 +0100

    Add ffmpeg installation to build script

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "=== Installing ffmpeg ==="
 apt-get update
 apt-get install -y ffmpeg
+echo "=== Installing Python dependencies ==="
 pip install -r requirements.txt
+echo "=== Checking ffmpeg ==="
+ffmpeg -version
